4b515890800d27f1bb332212ffd7f07394053971
manuelsanchezresendiz8-eng <manuelsanchezresendiz8@gmail.com>
2026-05-09 12:42:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,41 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-def home(request: Request):
+def index(request: Request):
     return templates.TemplateResponse(
         "index.html",
+        {"request": request}
+    )
+
+
+@app.get("/crm")
+def crm(request: Request):
+    return templates.TemplateResponse(
+        "crm.html",
+        {"request": request}
+    )
+
+
+@app.get("/limpieza")
+def limpieza(request: Request):
+    return templates.TemplateResponse(
+        "limpieza.html",
+        {"request": request}
+    )
+
+
+@app.get("/mantenimiento")
+def mantenimiento(request: Request):
+    return templates.TemplateResponse(
+        "mantenimiento.html",
+        {"request": request}
+    )
+
+
+@app.get("/insumos")
+def insumos(request: Request):
+    return templates.TemplateResponse(
+        "insumos.html",
         {"request": request}
     )
 

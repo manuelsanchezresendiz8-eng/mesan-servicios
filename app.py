b10b29db0c31a939710964cd5025b1f00c57d227
manuelsanchezresendiz8-eng <manuelsanchezresendiz8@gmail.com>
2026-05-08 20:23:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,22 +6,22 @@ import os
 
 app = FastAPI()
 
-# Esto detecta donde está parado el archivo y busca la carpeta templates ahí
 base_dir = os.path.dirname(os.path.realpath(__file__))
 templates = Jinja2Templates(directory=os.path.join(base_dir, "templates"))
 
+# Rutas corregidas para evitar el error de 'tuple'
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
 
 @app.get("/limpieza", response_class=HTMLResponse)
 async def limpieza(request: Request):
-    return templates.TemplateResponse("limpieza.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="limpieza.html")
 
 @app.get("/mantenimiento", response_class=HTMLResponse)
 async def mantenimiento(request: Request):
-    return templates.TemplateResponse("mantenimiento.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="mantenimiento.html")
 
 @app.get("/insumos", response_class=HTMLResponse)
 async def insumos(request: Request):
-    return templates.TemplateResponse("insumos.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="insumos.html")

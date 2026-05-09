22448dfb2a34bb02376365418952db7a6c99d360
manuelsanchezresendiz8-eng <manuelsanchezresendiz8@gmail.com>
2026-05-09 12:09:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
 @app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
-    )
-
+def home():
+    return {"status": "MESAN OK"}
 
 @app.get("/health")
 def health():

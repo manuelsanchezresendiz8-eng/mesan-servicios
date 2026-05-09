9c2fb6f09740242838c808013aec61644c785e96
manuelsanchezresendiz8-eng <manuelsanchezresendiz8@gmail.com>
2026-05-08 20:36:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
 
 app = FastAPI()
-
 base_dir = os.path.dirname(os.path.realpath(__file__))
 templates = Jinja2Templates(directory=os.path.join(base_dir, "templates"))
 
-# Montar estáticos para CSS y JS
-if os.path.exists(os.path.join(base_dir, "static")):
-    app.mount("/static", StaticFiles(directory=os.path.join(base_dir, "static")), name="static")
-
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    # Esta es tu Landing Page principal
     return templates.TemplateResponse(request=request, name="index.html")
 
 @app.get("/limpieza", response_class=HTMLResponse)
@@ -30,7 +23,6 @@ async def mantenimiento(request: Request):
 async def insumos(request: Request):
     return templates.TemplateResponse(request=request, name="insumos.html")
 
-# Si vas a meter el CRM, necesitamos una ruta para él
 @app.get("/crm", response_class=HTMLResponse)
 async def crm(request: Request):
     return templates.TemplateResponse(request=request, name="crm.html")

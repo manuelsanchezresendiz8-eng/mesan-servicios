ff632154d4f44b0a2ad6c7c79ec8c18cf33629e2
manuelsanchezresendiz8-eng <manuelsanchezresendiz8@gmail.com>
2026-05-09 12:52:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,46 +5,12 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-
 @app.get("/")
 def index(request: Request):
     return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
+        request=request,
+        name="index.html"
     )
-
-
-@app.get("/crm")
-def crm(request: Request):
-    return templates.TemplateResponse(
-        "crm.html",
-        {"request": request}
-    )
-
-
-@app.get("/limpieza")
-def limpieza(request: Request):
-    return templates.TemplateResponse(
-        "limpieza.html",
-        {"request": request}
-    )
-
-
-@app.get("/mantenimiento")
-def mantenimiento(request: Request):
-    return templates.TemplateResponse(
-        "mantenimiento.html",
-        {"request": request}
-    )
-
-
-@app.get("/insumos")
-def insumos(request: Request):
-    return templates.TemplateResponse(
-        "insumos.html",
-        {"request": request}
-    )
-
 
 @app.get("/health")
 def health():

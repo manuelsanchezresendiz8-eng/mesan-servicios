09080588e93e7f37838608c22c88ca60247f1d50
manuelsanchezresendiz8-eng <manuelsanchezresendiz8@gmail.com>
2026-05-09 13:21:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def index(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
+
 @app.get("/limpieza")
 def limpieza(request: Request):
     return templates.TemplateResponse(
         request=request,
         name="limpieza.html"
     )
+
+@app.get("/health")
+def health():
+    return {"status":"ok"}

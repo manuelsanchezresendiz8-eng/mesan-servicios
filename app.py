bf0bbec0645d4a482141f40e61b68d858335ef39
manuelsanchezresendiz8-eng <manuelsanchezresendiz8@gmail.com>
2026-05-08 20:56:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
 
@@ -9,20 +10,14 @@ templates = Jinja2Templates(directory=os.path.join(base_dir, "templates"))
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse(request=request, name="index.html")
-
-@app.get("/crm", response_class=HTMLResponse)
-async def crm(request: Request):
-    return templates.TemplateResponse(request=request, name="crm.html")
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/limpieza", response_class=HTMLResponse)
 async def limpieza(request: Request):
-    return templates.TemplateResponse(request=request, name="limpieza.html")
+    return templates.TemplateResponse("limpieza.html", {"request": request})
 
 @app.get("/mantenimiento", response_class=HTMLResponse)
 async def mantenimiento(request: Request):
-    return templates.TemplateResponse(request=request, name="mantenimiento.html")
+    return templates.TemplateResponse("mantenimiento.html", {"request": request})
 
-@app.get("/insumos", response_class=HTMLResponse)
-async def insumos(request: Request):
-    return templates.TemplateResponse(request=request, name="insumos.html")
+

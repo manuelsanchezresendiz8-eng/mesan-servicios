699424094f232bc2a4810821cb67eddd827c0a06
manuelsanchezresendiz8-eng <manuelsanchezresendiz8@gmail.com>
2026-05-08 20:15:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+# Esto detecta donde está parado el archivo y busca la carpeta templates ahí
+base_dir = os.path.dirname(os.path.realpath(__file__))
+templates = Jinja2Templates(directory=os.path.join(base_dir, "templates"))
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
